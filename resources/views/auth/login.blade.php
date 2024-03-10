d31fd2199106b8388ef29da33d1053552bcacaee
Nanguti <g.nanguti@gmail.com>
2024-03-10 23:00:20 +0300

    Login form enhancement

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,29 +6,29 @@
         @csrf
 
         <div>
-            <select id="login_method" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="login_method" required >
-                <option selected>Choose a Login Method</option>
+            <select id="login_method" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="login_method" required>
+                <option selected disabled>Select a Login Method</option>
                 <option value="email">Email</option>
                 <option value="fingerprint">Fingerprint</option>
             </select>
         </div>
 
         <!-- Email Address -->
-        <div class="mt-4" id="email_field">
+        <div class="mt-4 d-none" id="email_field">
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
-        <div class="mt-4" id="password_field">
+        <div class="mt-4 d-none" id="password_field">
             <x-input-label for="password" :value="__('Password')" />
             <x-text-input id="password" class="block mt-1 w-full" type="password" name="password"
                 required autocomplete="current-password" />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <div class="mt-4" id="fingerprint_field">
+        <div class="mt-4 d-none" id="fingerprint_field">
             <x-input-label for="fingerprint" :value="__('Fingerprint')" />
             <x-text-input id="fingerprint" class="block mt-1 w-full" type="text" name="fingerprint" />
         </div>
@@ -71,6 +71,10 @@
             const fingerprintField = document.getElementById('fingerprint_field');
             const fingerprintInput = document.getElementById('fingerprint');
 
+            // Hide email, password, and fingerprint fields initially
+            emailField.classList.add('d-none');
+            passwordField.classList.add('d-none');
+            fingerprintField.classList.add('d-none');
 
             if (loginMethodSelect && emailField && fingerprintField) {
                 loginMethodSelect.addEventListener('change', function() {
